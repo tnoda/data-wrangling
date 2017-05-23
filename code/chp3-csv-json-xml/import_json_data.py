92dedd9e62bc26c3d7d030256232edd34c27fbb6
Takahiro Noda <takahiro.noda+github@gmail.com>
2017-05-23 20:11:40 +0900

    Read a JSON file with Pandas

--- code/chp3-csv-json-xml/import_json_data.py
+++ code/chp3-csv-json-xml/import_json_data.py
@@ -1,8 +1,16 @@
-import json
+import pandas as pd
 
-json_data = open('data-text.json').read()
 
-data = json.loads(json_data)
+json_data = pd.read_csv('../../data/chp3/data-text.csv')
+for item in json_data.to_dict('record'):
+    print(item)
 
-for item in data:
-    print item
+
+
+
+
+
+
+
+
+
